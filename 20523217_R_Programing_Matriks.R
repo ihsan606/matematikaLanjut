986d25ca255aad2bde672db230d43b45a3a6e63b
ihsan606 <ihsansyafiul@gmail.com>
2021-03-06 14:32:35 +0700

    mtk lanjut

---
+++ 20523217_R_Programing_Matriks.R
@@ -0,0 +1,18 @@
+#Muhammad Ihsan Syafiul 
+?sample
+values <- sample(1:1000,100,FALSE)
+values
+G <- matrix(values,10,10,TRUE)
+G
+H <- t(G)
+H
+J <- G + H
+J
+det(G)
+det(H)
+det(J)
+K<-cbind(G[,1:5],J[,1:5])
+K
+G%%solve(G) 
+
+
